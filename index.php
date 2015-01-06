ca04827ca6d4898b704df94dee992689f518259e
alezyy <alezyy@yahoo.com>
2015-01-06 16:15:49 -0500

    add text

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ a:hover { color: #333; text-decoration: none; }
 <article>
 <h2>We&rsquo;ll be back soon!</h2>
 <div>
-<p>Sorry for the inconvenience but we&rsquo;re <h4>We are performing some maintenance at the moment....</h4>. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+<p>We are so Sorry for the inconvenience but we&rsquo;re <h4>We are performing some maintenance at the moment....</h4>. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
 </div>
 </article> 
 
